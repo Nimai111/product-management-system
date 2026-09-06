eef3e1037a8b63a65d9526070ee7129f2091f32f
Nimai111 <nimaicharan00000@gmail.com>
2026-09-06 14:00:33 +0530

    chore: initialize Spring Boot project

---
+++ src/test/java/com/nimai/product_management_system/ProductManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nimai.product_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
