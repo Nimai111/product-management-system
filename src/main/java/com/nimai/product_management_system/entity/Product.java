ff1a830ee84315b2ba109759cb5ab834cfad69e3
Nimai111 <nimaicharan00000@gmail.com>
2026-09-06 20:38:44 +0530

    feat: add product entity

---
+++ src/main/java/com/nimai/product_management_system/entity/Product.java
@@ -0,0 +1,29 @@
+package com.nimai.product_management_system.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Column(nullable = false)
+    private Integer quantity;
+}
